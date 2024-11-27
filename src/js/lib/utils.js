1f89ea4e3a1ce9a3c9685e38b7b98ed5ab08903e
goshabty <goshabty@gmail.com>
2024-11-27 13:11:43 +0500

    Show minutes when meetings are closer than 5 hours
    addon from damirault

--- src/js/lib/utils.js
+++ src/js/lib/utils.js
@@ -39,7 +39,7 @@ export function sec2time (timeInSeconds) {
   const minutes = Math.floor(time / 60) % 60
   const seconds = Math.floor(time - minutes * 60)
 
-  if (hours > 1) return `${sign}${hours}h`
+  if (hours > 4) return `~${sign}${hours}h`
   if (hours > 0 && minutes === 0) return `${sign}${hours}h 0m`
   if (hours > 0) return `${sign}${hours}h ${minutes}m`
   if (minutes > 0) return `${sign}${minutes}m ${seconds}s`
@@ -90,8 +90,10 @@ export function trimForMarquee (text, width, start = 0, lines = 1) {
   if (lines > 1) {
     let lineStart = start + width
     for (let i = 1; i < lines; i++) {
-      segment = segment.concat('\n', text.slice(lineStart, (lineStart + width)))
-      lineStart = lineStart + width
+      if (lineStart < text.length){
+        segment = segment.concat('\n', text.slice(lineStart, (lineStart + width)))
+        lineStart = lineStart + width
+      }
     }
   }
 
